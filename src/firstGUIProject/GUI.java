7f3344b2efcd83bdfdcee0332d77f5a4e241c967
hansika <hansikakgupta@gmail.com>
2020-06-19 11:32:06 +0530

    Creating 2 panels

--- src/firstGUIProject/GUI.java
+++ src/firstGUIProject/GUI.java
@@ -8,7 +8,8 @@ public class GUI {
 	public GUI() {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
 
 	}
 
