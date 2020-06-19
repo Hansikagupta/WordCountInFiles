d56ebd62e7aba5cdd5ab7d74ace56f8fc239bcfe
hansika <hansikakgupta@gmail.com>
2020-06-19 11:18:49 +0530

    Initial commit

---
+++ src/firstGUIProject/GUI.java
@@ -0,0 +1,15 @@
+package firstGUIProject;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+
+	public GUI() {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+
+	}
+
+}
