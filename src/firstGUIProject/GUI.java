44a32bed4f7ac9ae0612ff3633e0d73a0ad771b1
hansika <hansikakgupta@gmail.com>
2020-06-19 13:56:15 +0530

    wordCount Printed, working
    Needs UI improvement

--- src/firstGUIProject/GUI.java
+++ src/firstGUIProject/GUI.java
@@ -5,29 +5,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
 public class GUI {
-
-
-	
 	  public static void main(String args[]) throws IOException{
 	       JFrame frame = new JFrame("My First GUI");
-			JPanel panel1 = new JPanel();
-
+	       JPanel panel1 = new JPanel();
 	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       frame.setSize(300,300);
-	       
+	       frame.setSize(300,300);	       
 	        String word;
 			word = JOptionPane.showInputDialog("Word");
-			
-			
-
 			JFileChooser j1 = new JFileChooser();
 			j1.showSaveDialog(null);
 			JFileChooser j2 = new JFileChooser();
@@ -47,10 +38,18 @@ public class GUI {
 	        // file until a null is returned 
 	        while((line1 = reader1.readLine()) != null) 
 	        { 
+	        	StringTokenizer st = new StringTokenizer(line1);
+	        	while(st.hasMoreTokens())
+	        	{
+	        		if(word.equalsIgnoreCase(st.nextToken()))
+	        		{
+	        			wordCountF1++;
+	        		}
+	        	}
 	           // Search Word Instances
 	        } 
 	          
-	        System.out.println("Total word count in file 1= " + wordCountF1); 
+	        System.out.println("count of " + word + " in file 2 = " + wordCountF1); 
 	        
 		        FileInputStream fileStream2 = new FileInputStream(file2); 
 		        InputStreamReader input2 = new InputStreamReader(fileStream2); 
@@ -65,13 +64,23 @@ public class GUI {
 		        // file until a null is returned 
 		        while((line2 = reader2.readLine()) != null) 
 		        { 
+		        	StringTokenizer st = new StringTokenizer(line2);
+		        	while(st.hasMoreTokens())
+		        	{
+		        		if(word.equalsIgnoreCase(st.nextToken()))
+		        		{
+		        			wordCountF2++;
+		        		}
+		        	}
 		           // Search Word Instances
 		        } 
 		          
-		        System.out.println("Total word count in file 2= " + wordCountF2); 
-	       
+		        System.out.println("count of " + word + " in file 2 = " + wordCountF2); 
+		        reader1.close();
+		        reader2.close();
+		        JOptionPane.showMessageDialog(null, "count of " + word + " in file 1 = " + wordCountF1+"\ncount of " + word + " in file 2 = " + wordCountF2, "Results", JOptionPane.PLAIN_MESSAGE );
 	     
-	       frame.setVisible(true);
+//	       frame.setVisible(true);
 	    }
 
 }
